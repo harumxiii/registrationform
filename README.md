ac0de464a38ca4bf3a7e9cfe96163132bf81c487
harumxiii <nicolas.alexaven@gmail.com>
2025-02-14 23:03:15 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# registrationform
+ regform
---
+++ src/main/java/com/mycompany/registrationform/Registrationform.java
@@ -0,0 +1,12 @@
+package com.mycompany.registrationform;
+
+/**
+ *
+ * @author Veneriza Alexa
+ */
+public class Registrationform {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
